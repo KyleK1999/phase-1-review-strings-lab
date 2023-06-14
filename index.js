14f9d680c8444358208e6221ff88485104dd9012
Kyle <kylekibbe99@gmail.com>
2023-06-13 19:22:40 -0700

    Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+// Write your code in this file!"
+String ("currentUser")
+
+const currentUser = "name";
+
+const welcomeMessage = "Welcome to Flatbook, ${name}!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${NAME}!";
+
+const shortGreeting = "Welcome, ${n} !"
